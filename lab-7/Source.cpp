c960fb7aec5fbf6cc20d44e42a49db4d5b4336f4
Qdea <58660439+Qdea@users.noreply.github.com>
2020-05-07 16:36:03 +0300

    Update Source.cpp

--- lab-7/Source.cpp
+++ lab-7/Source.cpp
@@ -15,7 +15,7 @@ int main()
 	{
 		cout << endl << "Enter the command:" << endl;
 		cin >> command;
-		if (command == "insert-end")
+		if (!(command != "insert-end"))
 		{
 			int x;
 			cin >> x;
@@ -24,14 +24,14 @@ int main()
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "delete-end")
+		else if (!(command != "delete-end"))
 		{
 			buffer.delete_end();
 			cout << "Total buffer: ";
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "insert-start")
+		else if (!(command != "insert-start"))
 		{
 			int x;
 			cin >> x;
@@ -40,14 +40,14 @@ int main()
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "delete-start")
+		else if (!(command != "delete-start"))
 		{
 			buffer.delete_start();
 			cout << "Total buffer: ";
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "insert-it")
+		else if (!(command != "insert-it"))
 		{
 			int index;
 			int value;
@@ -58,31 +58,30 @@ int main()
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "delete-it")
+		else if (!(command != "delete-it"))
 		{
 			int x;
-			cout << "Enter value to delete:" << endl;
 			cin >> x;
-			buffer.delete_it(find(buffer.begin(), buffer.end(), x));
+			buffer.delete_it(Go(buffer.begin(), buffer.end(), x));
 			cout << "Total buffer: ";
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "find-start")
+		else if (!(command != "find-start"))
 		{
 			cout << buffer[0];
 		}
-		else if (command == "find-end") {
+		else if (!(command != "find-end")) {
 			cout << buffer[size - 1];
 		}
-		else if (command == "find-id")
+		else if (!(command != "find-id"))
 		{
 			int id;
 			cin >> id;
 			cout << "Your element whith id=" << id << ": ";
 			cout << buffer[id];
 		}
-		else if (command == "resize")
+		else if (!(command != "resize"))
 		{
 			int _size;
 			cout << "Enter new size:" << endl;
@@ -93,18 +92,18 @@ int main()
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "print")
+		else if (!(command != "print"))
 		{
 			cout << "Total buffer: ";
 			for (int i = 0; i < buffer.size(); i++)
 				cout << buffer[i] << " ";
 		}
-		else if (command == "print-it")
+		else if (!(command != "print-it"))
 		{
 			for (auto it = buffer.begin() + 1; it != buffer.end(); it++)
 				cout << *it << " ";
 		}
-		else if (command == "stop") {
+		else if (!(command != "stop")) {
 			cout <<endl << "Good bye";
 			exit(0);
 		}
@@ -116,11 +115,12 @@ int main()
 			cout << "3)insert-it, delete-it" << endl;
 			cout << "4)find-start, find-end " << endl;
 			cout << "5)find-id" << endl;
-			cout << "6) resize" << endl; 
-			cout << "7) print" << endl; 
-			cout << "8) print-it" << endl;
-			cout << "9) stop" << endl;
-			cout << "To start enter command insert-start and enter your value";
+			cout << "6)resize" << endl; 
+			cout << "7)print" << endl; 
+			cout << "8)print-it" << endl;
+			cout << "9)stop" << endl;
+			cout << endl;
+			cout << "To start enter command 'insert-start' and enter your value";
 		}
 	}
 }
